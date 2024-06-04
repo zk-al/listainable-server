3515f641f9d3184d6184b9440a8609aecef581fe
Zack Lamarche <lamarche.zack@gmail.com>
2024-06-04 16:50:14 -0700

    Add list POST request :hammer:

--- routes/list.js
+++ routes/list.js
@@ -54,11 +54,24 @@ Parameters:
 
 const express = require("express");
 const router = express.Router();
-// const knex = require("knex")(require("../knexfile"));
+const knex = require("knex")(require("../knexfile"));
 
 // Change to POST
-router.get("/", (req, res) => {
-  res.status(200).send("List Page: ADD products to list");
+router.post("/", async (req, res) => {
+  // res.status(200).send("List Page: ADD products to list");
+  console.log(req.body);
+
+  try {
+    const [newListId] = await knex("user_list").insert(req.body);
+    const newListItem = await knex("user_list")
+      .where({ id: newListId })
+      .first();
+
+    res.status(201).json(newListItem);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to add item" });
+  }
 });
 
 // Change to PUT
