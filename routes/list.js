04c675ef2407ad2dca8a048fa1719012d6f80f73
Zack Lamarche <lamarche.zack@gmail.com>
2024-06-04 17:23:32 -0700

    Clean up comments :wastebasket:

--- routes/list.js
+++ routes/list.js
@@ -1,62 +1,7 @@
-/*
-
-POST /list
-
-Add new list item
-
-Parameters:
-
-    token: JWT of logged in user
-    product-name
-    ecoscore
-    quantity: input when adding item to list
-
-Example Response
-
-{
-    "id": 1
-    "product-name": "Nutella",
-    "product-image": "image-url",
-    "ecoscore": "c",
-    "quantity": 4,
-}
-
-PUT /list/:id
-
-Edit list item
-
-Parameters:
-
-    token: JWT of logged in user
-    id: list item id
-    quantity: amount of the item
-
-Example Response:
-
-{
-    "id": 1
-    "product-name": "Nutella",
-    "product-image": "image-url",
-    "ecoscore": "c",
-    "quantity": 2,
-}
-
-DELETE /list/:id
-
-Delete list item
-
-Parameters:
-
-    token: JWT of logged in user
-    id: list item id
-
-*/
-
 const express = require("express");
 const router = express.Router();
 const knex = require("knex")(require("../knexfile"));
 
-// Change to POST
 router.post("/", async (req, res) => {
   try {
     const [newListId] = await knex("user_list").insert(req.body);
@@ -70,7 +15,6 @@ router.post("/", async (req, res) => {
   }
 });
 
-// Change to PUT
 router.put("/:id", async (req, res) => {
   try {
     await knex("user_list")
@@ -81,10 +25,7 @@ router.put("/:id", async (req, res) => {
   }
 });
 
-// Change to DELETE
 router.delete("/:id", async (req, res) => {
-  // res.status(200).send(`List Page: DELETE products to list ${req.params.id}`);
-
   try {
     await knex("user_list").where({ id: req.params.id }).del();
     res.status(204).end();
