45ec5e4739b62ada0b6374caa1aac59c82839817
Zack Lamarche <lamarche.zack@gmail.com>
2024-06-04 17:30:17 -0700

    Clean up comments :wastebasket:

--- routes/products.js
+++ routes/products.js
@@ -1,24 +1,3 @@
-/*
-
-GET /product/category
-
-Get products of a certain category to display on home page
-
-Parameters:
-    category
-
-Example Response:
-
-{
-    "product-name": "Nutella",
-    "product-image": "image-url",
-    "category": "Condiment"
-    "eco-score": "c",
-    "nutri-score": "d",
-}
-
-*/
-
 const express = require("express");
 const router = express.Router();
 const knex = require("knex")(require("../knexfile"));
