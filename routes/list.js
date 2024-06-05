bd0302049b6bbb4ce333da1cc54259c49cde9217
Zack Lamarche <lamarche.zack@gmail.com>
2024-06-05 15:45:49 -0700

    Add GET user list :hammer:

--- routes/list.js
+++ routes/list.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const listController = require("../controllers/list-controller");
 
-router.route("/").post(listController.addItem);
+router.route("/").get(listController.getList).post(listController.addItem);
 
 router
   .route("/:id")
