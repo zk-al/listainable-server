54eb9f168cc1c8b1e2aeffdc277261acd2c882c6
Zack Lamarche <lamarche.zack@gmail.com>
2024-06-04 17:43:58 -0700

    Add list controller :recycle:

--- routes/list.js
+++ routes/list.js
@@ -1,37 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const knex = require("knex")(require("../knexfile"));
+const listController = require("../controllers/list-controller");
 
-router.post("/", async (req, res) => {
-  try {
-    const [newListId] = await knex("user_list").insert(req.body);
-    const newListItem = await knex("user_list")
-      .where({ id: newListId })
-      .first();
+router.route("/").post(listController.addItem);
 
-    res.status(201).json(newListItem);
-  } catch (error) {
-    res.status(500).json({ error: "Failed to add item" });
-  }
-});
-
-router.put("/:id", async (req, res) => {
-  try {
-    await knex("user_list")
-      .where({ id: req.params.id })
-      .update({ quantity: req.body.quantity });
-  } catch (error) {
-    res.status(500).json({ error: "Failed to update" });
-  }
-});
-
-router.delete("/:id", async (req, res) => {
-  try {
-    await knex("user_list").where({ id: req.params.id }).del();
-    res.status(204).end();
-  } catch (error) {
-    res.status(500).json({ error: "Failed to delete" });
-  }
-});
+router
+  .route("/:id")
+  .put(listController.editItem)
+  .delete(listController.deleteItem);
 
 module.exports = router;
