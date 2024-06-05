74dc1b1418fb7517be8bc315097d77a0de73723d
Zack Lamarche <lamarche.zack@gmail.com>
2024-06-04 23:07:15 -0700

    Add single product GET :hammer:

--- routes/products.js
+++ routes/products.js
@@ -4,11 +4,22 @@ const knex = require("knex")(require("../knexfile"));
 
 router.get("/", async (req, res) => {
   try {
-    const data = await knex("products");
-    res.status(200).json(data);
+    const allProducts = await knex("products");
+    res.status(200).json(allProducts);
   } catch (error) {
     res.status(500).send({ error: "Failed to fetch data" });
     console.log(`${error}`);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const singleProduct = await knex("products")
+      .where({ id: req.params.id })
+      .first();
+    res.status(200).json(singleProduct);
+  } catch (error) {
+    res.status(500).json({ message: "error getting product" });
   }
 });
 
